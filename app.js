cd376d29297558680a3d0b633c9c226498dbdd6f
aminbabu12 <badrulaminbabu@gmail.com>
2025-02-02 11:07:03 +0600

    required done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Interactive functionallity
+
 function showForm(type){
   document.getElementById('registerForm').classList.add('d-none');
   document.getElementById('loginForm').classList.add('d-none');
@@ -7,5 +9,27 @@ function showForm(type){
     document.getElementById('registerForm').classList.remove('d-none');
   }else if(type === 'login'){
     document.getElementById('loginForm').classList.remove('d-none');
+  }
+}
+
+
+// Register functionallity
+
+function register(){
+  const fullName = document.getElementById('regFullname').value;
+  const email = document.getElementById('regEmail').value;
+  const password = document.getElementById('regPassword').value;
+
+  if (!fullName){
+    alert('Please enter your name.');
+    return;
+  }
+  if (!email){
+    alert('Please enter your email.');
+    return;
+  }
+  if (!password){
+    alert('Please enter your password.');
+    return;
   }
 }
